f94d22b981bd7e53b4445187b7a593fef6abc88b
Anderson <andemartinezdev@gmail.com>
2022-04-15 11:22:44 -0500

    17 - Funciones en conversor
    - Se usaron funciones para refactorizar el codigo repetido del conversor.

--- 1_Condicionales/2_conversor.py
+++ 1_Condicionales/2_conversor.py
@@ -6,28 +6,28 @@ Bienvenido al conversor de monedas 😗
 
 Elige una opcion: """
 
+def calculo(pesos, valor_dolar):
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    return dolares
+
 opcion = int(input(menu))
 
 if opcion == 1:
-    pesos = int(input("Ingresa los COP que quieres convertir a Dolares"))
+    pesos = int(input("Ingresa los COP que quieres convertir a Dolares "))
     valor_dolar = 3760
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " Dolares")
+    print("Tienes $" + calculo(pesos, valor_dolar) + " Dolares")
 elif opcion == 2:
-    pesos = int(input("Ingresa los ARS que quieres convertir a Dolares"))
+    pesos = int(input("Ingresa los ARS que quieres convertir a Dolares "))
     valor_dolar = 113.98
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " Dolares")
+    print("Tienes $" + calculo(pesos, valor_dolar) + " Dolares")
 elif opcion == 3:
-    pesos = int(input("Ingresa los MXP que quieres convertir a Dolares"))
+    pesos = int(input("Ingresa los MXP que quieres convertir a Dolares "))
     valor_dolar = 19.97
-    dolares = pesos / valor_dolar
-    dolares = round(dolares, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " Dolares")
+    print("Tienes $" + calculo(pesos, valor_dolar) + " Dolares")
 else:
     print("Elige alguna opcion entre 1 y 3")
+
+
+
