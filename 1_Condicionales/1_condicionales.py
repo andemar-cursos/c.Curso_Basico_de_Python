06af6d4571410b39c92507cf542722b6c7bf2622
Anderson <andemartinezdev@gmail.com>
2022-04-15 10:48:41 -0500

    14 - Condicionales
    Condicional if-elif-else.

---
+++ 1_Condicionales/1_condicionales.py
@@ -0,0 +1,15 @@
+# edad = int(input("Escribe tu edad:"))
+#
+# if edad > 17:
+#     print("Eres mayor de edad")
+# else:
+#     print("Eres menos de edad")
+
+numero = int(input("Escribe un numero"))
+
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5:
+    print("Es 5")
+else:
+    print("Es menos a 5")
